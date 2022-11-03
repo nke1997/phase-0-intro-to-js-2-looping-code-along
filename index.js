b76b672bcd02272a0715340a749ca4a00bf3f346
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-03 11:47:22 -0400

    commit tree 789657745854dd78f0876aef8d53ad096045da18

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(giftGivers) {
+    let thankYouNotes = [];
+    for (let i = 0; i < giftGivers.length; i++) {
+        thankYouNotes.push(`Thank you, ${giftGivers[i]}, for the wonderful surprise gift!`);
+    };
+    return thankYouNotes;
+};
+
+function countDown(numberInput) {
+    for (let i = numberInput; i >= 0; i--) {
+        console.log(i);
+    };
+};
